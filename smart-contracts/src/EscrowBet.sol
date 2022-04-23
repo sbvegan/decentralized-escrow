103b1786d8bc2ae0b6807469c8a9e3ba23477197
soyboy <sbvegan@protonmail.com>
2022-04-22 17:55:10 -0700

    adding basic initialization

--- smart-contracts/src/EscrowBet.sol
+++ smart-contracts/src/EscrowBet.sol
@@ -2,24 +2,53 @@
 pragma solidity ^0.8.0;
 
 contract EscrowBet {
-    // Wager parameters
-    // participants
-    address bull;
-    address bear;
-    // asset
-    // chainlink data feed to get the price
-    // anchor price bull wins if asset is > anchor at expiration
-    // and vice versa for the bear
-    // payday / expiration time
-    // enum for contract state: INITIALIZED, ACTIVE, COMPLETE
+    // parameters
+    address public bull; // bets the price will be higher than anchor at expiration
+    address public bear; // bets the price will be lower than anchor at expiration
+    address public assetDatafeed; // chainlink data feed to get the price
+    int256 public anchorPrice; // anchor > expirationPrice --> bear and vice versa
+    uint256 public initTimestamp; // timestamp the bet was initialized
+    uint256 public activeTimestamp; // timestamp the bet was fully funded
+    uint256 public paydayTimestamp; // payday / expiration time
+    enum State {
+        INITIALIZED,
+        ACTIVE,
+        COMPLETE
+    } // contract state
+    State public state;
 
-    // constructor that allows one of the participants to initialize the bet
+    /** The constructor initializes the bet
+    bool _bull - true --> bull && false --> bear
+    address _assetDatafeed - address of the chainlink datafeed
+    int256 _anchorPrice - the high or low point
+    uint256 _paydayTimestamp - payday / expiration time
+    */
+    constructor(
+        bool _bull,
+        address _assetDatafeed,
+        int256 _anchorPrice,
+        uint256 _paydayTimestamp
+    ) {
+        // TODO: add parameter checks
+        initTimestamp = block.timestamp;
+        if (_bull) {
+            bull = msg.sender;
+        } else {
+            bear = msg.sender;
+        }
+        assetDatafeed = _assetDatafeed;
+        anchorPrice = _anchorPrice;
+        paydayTimestamp = _paydayTimestamp;
+        state = State.INITIALIZED;
+    }
 
-    // checkUpkeep
+    // TODO: checkUpkeep
 
-    // performUpkeep
+    // TODO: performUpkeep
 
-    // withdraw functionality
+    // TODO: deposit functionality
 
-    // change state
+    // TODO: withdraw functionality
+
+    // TODO: change state
 }
--- smart-contracts/test/EscrowBet.t.sol
+++ smart-contracts/test/EscrowBet.t.sol
@@ -6,13 +6,31 @@ import "forge-std/Test.sol";
 import "src/EscrowBet.sol";
 
 contract TestEscrowBet is Test {
-    EscrowBet c;
+    EscrowBet escrowBet;
+    address deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84; // foundry def
 
-    function setUp() public {
-        c = new EscrowBet();
+    // event log_named_address(string key, address val);
+
+    function testValidBullInit() public {
+        bool bull = true;
+        address ethusdDatafeed = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+        int256 anchorPrice = 3000;
+        uint256 may27th = 1653609600;
+        escrowBet = new EscrowBet(bull, ethusdDatafeed, anchorPrice, may27th);
+        assertEq(deployer, escrowBet.bull());
+        assertEq(ethusdDatafeed, escrowBet.assetDatafeed());
+        assertEq(anchorPrice, escrowBet.anchorPrice());
+        assertEq(may27th, escrowBet.paydayTimestamp());
     }
 
-    function testBar() public {
-        assertEq(uint256(2), 2, "ok");
+    function testValidBearInit() public {
+        bool bear = false;
+        address ethusdDatafeed = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+        int256 anchorPrice = 3000;
+        uint256 may27th = 1653609600;
+        escrowBet = new EscrowBet(bear, ethusdDatafeed, anchorPrice, may27th);
+        assertEq(deployer, escrowBet.bear());
+        assertEq(ethusdDatafeed, escrowBet.assetDatafeed());
+        assertEq(anchorPrice, escrowBet.anchorPrice());
     }
 }
