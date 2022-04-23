84ceefd25778efd6b336261537149a1c6d477b0c
soyboy <sbvegan@protonmail.com>
2022-04-22 23:49:51 -0700

    changed config

--- smart-contracts-hardhat/hardhat.config.js
+++ smart-contracts-hardhat/hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +17,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
+  networks: {
+    kovan: {
+      url: process.env.KOVAN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
