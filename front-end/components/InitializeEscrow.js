8641e0ab56ae968fca7dba2c46618c54b066d7c0
soyboy <sbvegan@protonmail.com>
2022-05-12 22:55:40 -0700

    adding escrow creation from

--- front-end/components/InitializeEscrow.js
+++ front-end/components/InitializeEscrow.js
@@ -1,7 +1,57 @@
 import { useWeb3Contract } from "react-moralis";
+import { Form } from "web3uikit"
+import { useState } from "react"
 
 export default function InitializeEscrow() {
-    return (<div>
-        Init Escrow
-    </div>)
+
+    // TODO: add state for smart contract initialization parameters
+    const [assetPriceFeed, setAssetPriceFeed] = useState("")
+
+    return (
+        <div>
+            <Form
+                buttonConfig={{
+                    onClick: function noRefCheck() { },
+                    theme: 'primary'
+                }}
+                data={[
+                    {
+                        inputWidth: '100%',
+                        name: 'Chainlink Asset Price Feed Address',
+                        type: 'text',
+                        // TODO: add address validation regex
+                        // validation: {
+                        //    regExp: '',
+                        //    required: true
+                        // },
+                        value: ''
+                    },
+                    {
+                        inputWidth: '100%',
+                        name: 'Wager Size',
+                        type: 'number',
+                        value: ''
+                    },
+                    {
+                        inputWidth: '100%',
+                        name: 'Anchor Price (the high or low point)',
+                        type: 'number',
+                        // TODO: needs validation for no negative numbers
+                        // validation: {
+                        //     required: true
+                        // },
+                        value: ''
+                    },
+                    {
+                        name: 'Payday (when the day the bet ends )',
+                        type: 'date',
+                        value: ''
+                    }
+
+                ]}
+                onSubmit={function noRefCheck() { }}
+                title="Escrow creation form"
+            />
+        </div>
+    )
 }
