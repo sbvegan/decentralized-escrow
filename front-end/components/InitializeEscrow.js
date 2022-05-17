75327181da2f72757f947d0f8bb61d4ad403a94e
soyboy <sbvegan@protonmail.com>
2022-05-16 20:42:21 -0700

    creating escrow contracts from the UI

--- front-end/components/InitializeEscrow.js
+++ front-end/components/InitializeEscrow.js
@@ -1,21 +1,23 @@
-import { useWeb3Contract } from "react-moralis";
-import { Form } from "web3uikit"
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { Form, Input, DatePicker, Button } from "web3uikit"
 import { useState } from "react"
 import { abi } from "../constants/escrow-factory-abi.json"
+
 
 // on kovan
 const ESCROW_FACTORY_ADDRESS = "0x6BF064fC1138f30744Edf593160Aea10754a6532";
 
 export default function InitializeEscrow() {
-
-    const [assetPriceFeed, setAssetPriceFeed] = useState("")
-    const [wager, setWager] = useState("")
-    const [anchorPrice, setAnchorPrice] = useState("")
-    const [paydayTimestamp, setPaydayTimestamp] = useState("")
+    const { Moralis } = useMoralis();
+    const [assetPriceFeed, setAssetPriceFeed] = useState("0x9326BFA02ADD2366b30bacB125260Af641031331")
+    const [wager, setWager] = useState(Moralis.Units.Token("0.01", "18"))
+    const [anchorPrice, setAnchorPrice] = useState("300000000000")
+    const [paydayTimestamp, setPaydayTimestamp] = useState("1653609600")
 
     const { runContractFunction: createEscrow } = useWeb3Contract({
+        chain: "kovan",
         abi: abi,
-        address: ESCROW_FACTORY_ADDRESS,
+        contractAddress: ESCROW_FACTORY_ADDRESS,
         functionName: "createEscrow",
         params: {
             _assetPriceFeed: assetPriceFeed,
@@ -25,73 +27,58 @@ export default function InitializeEscrow() {
         }
     })
 
-    const printParameters = () => {
-        console.log(`Asset Price Feed: ${assetPriceFeed}`)
-        console.log(`Wager: ${wager}`)
-        console.log(`Anchor Price: ${anchorPrice}`)
-        console.log(`Payday Timestamp: ${paydayTimestamp}`)
+    const handleComplete = async (tx) => {
+        console.log("COMPLETE")
     }
 
-    const handleCreation = async (data) => {
-        const formInput = data.data
-        setAssetPriceFeed(formInput[0].inputResult)
-        setWager(formInput[1].inputResult)
-        setAnchorPrice(formInput[2].inputResult)
-        setPaydayTimestamp(formInput[3].inputResult)
-        createEscrow()
+    const handleError = async (error) => {
+        console.error("ERROR")
+        console.log(error)
     }
 
+    const handleSuccess = async (tx) => {
+        console.log("SUCCESS")
+        console.log(tx)
+    }
 
 
     return (
         <div>
-            <Form
-                buttonConfig={{
-                    isLoading: false,
-                    loadingText: 'Creating Escrow...',
-                    text: 'Create Escrow',
-                    theme: 'primary'
+            <Input
+                label="Chainlink Price Feed Address"
+                name="Chainlink Price Feed Address"
+                onChange={(e) => setAssetPriceFeed(e.target.value)}
+            />
+            <Input
+                label="Wager Size"
+                name="Wager Size"
+                onChange={(e) => setWager(Moralis.Units.Token(e.target.value, "18"))}
+                type="number"
+            />
+            <Input
+                label="Anchor Price"
+                name="Anchor Price"
+                onChange={(e) => setAnchorPrice(e.target.value)}
+                type="number"
+            />
+            <DatePicker
+                id="payday-date-picker"
+                label="Payday"
+                onChange={(e) => setPaydayTimestamp(e.target.value)}
+            />
+            <Button
+                color="blue"
+                id="create-escrow-button"
+                onClick={async () => {
+                    await createEscrow({
+                        onComplete: handleComplete,
+                        onError: handleError,
+                        onSuccess: handleSuccess
+                    })
                 }}
-                data={[
-                    {
-                        inputWidth: '100%',
-                        name: 'Chainlink Asset Price Feed Address',
-                        type: 'text',
-                        // TODO: add address validation regex
-                        // validation: {
-                        //    regExp: '',
-                        //    required: true
-                        // },function noRefCheck() { }DO: needs validation for no negative numbers
-                        // validation: {
-                        //     required: true
-                        // },
-                        value: ''
-                    },
-                    {
-                        inputWidth: '100%',
-                        name: 'Wager Size',
-                        type: 'number',
-                        value: ''
-                    },
-                    {
-                        inputWidth: '100%',
-                        name: 'Anchor Price (the high or low point)',
-                        type: 'number',
-                        // TODO: needs validation for no negative numbers
-                        // validation: {
-                        //     required: true
-                        // },
-                        value: ''
-                    },
-                    {
-                        name: 'Payday (when the day the bet ends )',
-                        type: 'date',
-                        value: ''
-                    }
-
-                ]}
-                onSubmit={handleCreation}
-                title="Escrow creation form"
+                text="Create Escrow"
+                theme="colored"
+                type="button"
             />
         </div>
     )
